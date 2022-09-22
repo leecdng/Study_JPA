09e333e0746660c7fb2451ce24f17793cb645cc2
leecdng <leecdng@gmail.com>
2022-09-22 22:42:02 +0900

    엔티티 클래스 개발

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,18 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter
+public class Delivery {
+
+    @Id @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+}
